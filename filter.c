424afa7dd17ca61067c77db6cc3b19edbb7d19d0
Stefano Torneo <50771226+s-torneo@users.noreply.github.com>
2019-06-04 17:05:42 +0200

    Add files via upload

---
+++ filter.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+
+void my_packet_handler(u_char *args, const struct pcap_pkthdr *header, const u_char *packet){
+    struct ether_header *eth_header;
+    eth_header = (struct ether_header *) packet;
+    if (ntohs(eth_header->ether_type) != ETHERTYPE_IP) {
+        printf("Not an IP packet. Skipping...\n\n");
+        return;
+    }
+
+    printf("\nTotal packet available: %d bytes\n", header->caplen);
+    printf("Expected packet size: %d bytes\n", header->len);
+
+    /* Pointers to headers */
+    const u_char *ip_header;
+    const u_char *tcp_header;
+    const u_char *payload;
+
+    /* Header lengths in bytes */
+    int ethernet_header_length = 14;
+    int ip_header_length;
+    int tcp_header_length;
+    int payload_length;
+
+    /* Find start of IP header */
+    ip_header = packet + ethernet_header_length;
+    ip_header_length = ((*ip_header) & 0x0F);
+    ip_header_length = ip_header_length * 4;
+    printf("IP header length in bytes: %d\n", ip_header_length);
+    
+    u_char protocol = *(ip_header + 9);
+    if (protocol != IPPROTO_TCP) {
+        printf("Not a TCP packet\n\n");
+        return;
+    }
+   /* Add the ethernet and ip header length to the start of the packet
+       to find the beginning of the TCP header */
+    tcp_header = packet + ethernet_header_length + ip_header_length;
+
+    tcp_header_length = ((*(tcp_header + 12)) & 0xF0) >> 4;
+    tcp_header_length = tcp_header_length * 4;
+
+    /* Add up all the header sizes to find the payload offset */
+    int total_headers_size = ethernet_header_length+ip_header_length+tcp_header_length;
+
+    printf("Total size of headers: %d bytes\n", total_headers_size);
+    payload_length = header->caplen -(ethernet_header_length + ip_header_length + tcp_header_length);
+    printf("Payload size: %d bytes\n\n", payload_length);
+    payload = packet + total_headers_size;
+
+    uint32_t  len = *(payload);
+    len = ntohs(len);
+    printf("\nlen payload: %x\n",len);
+    if (payload_length > 0) {
+        const u_char *temp_pointer = payload;
+        int byte_count = 0;
+        while (byte_count++ < payload_length) {
+            printf("%c", *temp_pointer);
+            temp_pointer++;
+        }
+        printf("\n");
+    }
+    return;
+}
+
+int main(int argc, char **argv) {
+    char dev[] ="lo";
+    pcap_t *handle;
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    struct bpf_program filter;
+    char filter_exp[] = "port 22";
+    bpf_u_int32 subnet_mask, ip;
+
+    if (pcap_lookupnet(dev, &ip, &subnet_mask, error_buffer) == -1) {
+        printf("Could not get information for device: %s\n", dev);
+        ip = 0;
+        subnet_mask = 0;
+    }
+
+    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, error_buffer);
+    if (handle == NULL) {
+        printf("Could not open %s - %s\n", dev, error_buffer);
+        return 2;
+    }
+    if (pcap_compile(handle, &filter, filter_exp, 0, ip) == -1) {
+        printf("Bad filter - %s\n", pcap_geterr(handle));
+        return 2;
+    }
+    if (pcap_setfilter(handle, &filter) == -1) {
+        printf("Error setting filter - %s\n", pcap_geterr(handle));
+        return 2;
+    }
+    
+    pcap_loop(handle,-1, my_packet_handler,NULL);   
+    pcap_close(handle);
+
+    return 0;
+}
